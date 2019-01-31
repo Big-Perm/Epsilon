274e8fbbfbbfd9d6d2d960b1e3fe787f5787219a
Big-Perm <44013648+Big-Perm@users.noreply.github.com>
2019-01-30 19:00:45 -0700

    Add files via upload

---
+++ vctmp5596_454812.Authenticate.72f2f01d.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Epsilon
+{
+    class Authenticate
+    {
+        private static int OFFSET = 25;
+
+        public string Encrypt (string input)
+        {
+            byte[] asciivalues = Encoding.ASCII.GetBytes(input);
+            for (int i = 0; i < asciivalues.Length; i++)
+            {
+                asciivalues[i] += (byte)OFFSET;
+                while (asciivalues[i] > 122)
+                {
+                    asciivalues[i] -= 90;
+                }
+            }
+            return Encoding.ASCII.GetString(asciivalues);
+        }
+    }
+}
